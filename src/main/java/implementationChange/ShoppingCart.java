f60c3f5b43bf094e020c55322dd03ec083f07bbe
Cristian Suárez Vera <cristian.suarez@leanmind.es>
2019-04-16 11:02:29 +0100

    Create and add to `add` method new field `List<Integer>`

--- src/main/java/implementationChange/ShoppingCart.java
+++ src/main/java/implementationChange/ShoppingCart.java
@@ -1,10 +1,15 @@
 package implementationChange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
     private int price;
+    private List<Integer> prices = new ArrayList<>();
 
     public void add(int price) {
         this.price = price;
+        this.prices.add(price);
     }
 
     public int calculateTotalPrice() {
